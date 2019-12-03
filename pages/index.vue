f6e627cb9e17630c3596fa274ffab76576cc5856
Thet Min Khant <tkt11497@gmail.com>
2019-12-03 10:28:23 +0630

    fixingmsger

--- pages/index.vue
+++ pages/index.vue
@@ -46,21 +46,7 @@
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import productlist from '~/components/product/productlist'
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : 'your-app-id',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.11'
-    });
-  };
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+
 
 export default {
   data(){
@@ -96,7 +82,25 @@ export default {
     //   this.show=!this.show
     // }
   },
-
+  created(){
+    if(process.client){
+    window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '108481103963063',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+  };
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  }
+  }
   
 }
 </script>
