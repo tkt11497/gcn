a9fe3c46c4b14c9c7dccc7d5cd93fab15b59cd40
Thet Min Khant <tkt11497@gmail.com>
2019-12-05 14:10:54 +0630

    hello

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,28 +56,15 @@ export default {
     modules: [
         'vue-scrollto/nuxt',
     ],
+    
+  buildModules: [
+    // Simple usage
+    '@nuxtjs/vuetify',
+  ],
     /*
      ** vuetify module configuration
      ** https://github.com/nuxt-community/vuetify-module
      */
-    vuetify: {
-        customVariables: ['~/assets/variables.scss'],
-        iconfont: 'mdi',
-        // theme: {
-        //     dark: true,
-        //     themes: {
-        //         dark: {
-        //             primary: colors.blue.darken2,
-        //             accent: colors.grey.darken3,
-        //             secondary: colors.amber.darken3,
-        //             info: colors.teal.lighten1,
-        //             warning: colors.amber.base,
-        //             error: colors.deepOrange.accent4,
-        //             success: colors.green.accent3
-        //         }
-        //     }
-        // }
-    },
     /*
      ** Build configuration
      */
